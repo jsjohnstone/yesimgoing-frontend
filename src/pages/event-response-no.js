831dd34615f21c7c85461147ef275c349bbe6479
Jamie Johnstone <j@miej.co>
2023-08-12 19:46:08 +0100

    Fix no message

--- src/pages/event-response-no.js
+++ src/pages/event-response-no.js
@@ -4,9 +4,8 @@ export default function EventResponse() {
 
     return (
                 <div>
-                    No!
-
-
+                    That's too bad. We'll miss you!<br /><br />
+                    <i>(This part doesn't work yet... please let the organiser know you can't make it)</i>
                 </div>
     );
 }
